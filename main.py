c5c1eaa8ed3feb7ab6d2f19c9bf19f70a2c9372f
Henrique Souto Maior <50162345+henrique-smr@users.noreply.github.com>
2024-08-13 22:44:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ class KeywordQueryEventListener(EventListener):
             return RenderResultListAction(items)
 
         items.append(ExtensionResultItem(
-            icon='images/icon.png',
+            icon='images/window.svg',
             name='Search for a window by name',
             description=f'Try searching for something longer than {MIN_LEN_TO_SEARCH} characters'
         ))
